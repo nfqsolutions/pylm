731bcfcabb471d0ddf792186df2c8ecdd653b92e
Guillem Borrell <guillemborrell@gmail.com>
2016-03-14 13:31:48 +0000

    Deleted pub and sub connections because there is no interest in subscription to topics when all connections have a given target.

--- pylm_ng/components/connections.py
+++ pylm_ng/components/connections.py
@@ -56,32 +56,6 @@ class PullConnection(ComponentInbound):
         )
 
 
-class SubConnection(ComponentInbound):
-    """
-    ReqRep is a component that connects a REQ socket to the broker, and a PULL
-    socket to an external service.
-    """
-    def __init__(self, name, listen_to, broker_address="inproc://broker",
-                 logger=None, messages=sys.maxsize):
-        """
-        :param name: Name of the component
-        :param listen_to: ZMQ socket address to listen to
-        :param broker_address: ZMQ socket address for the broker
-        :param logger: Logger instance
-        :param messages: Maximum number of inbound messages. Defaults to infinity.
-        :return:
-        """
-        super(SubConnection, self).__init__(
-            name,
-            listen_to,
-            zmq.SUB,
-            reply=False,
-            broker_address=broker_address,
-            logger=logger,
-            messages=messages
-        )
-
-
 class PushConnection(ComponentOutbound):
     """
     ReqPush is a component that connects a REQ socket to the broker, and a PUSH
@@ -108,32 +82,7 @@ class PushConnection(ComponentOutbound):
         )
 
 
-class PubConnection(ComponentOutbound):
-    """
-    Connection that PUBs from the broker
-    """
-    def __init__(self, name, listen_to, broker_address="inproc://broker",
-                 logger=None, messages=sys.maxsize):
-        """
-        :param name: Name of the connection
-        :param listen_to: ZMQ socket address to listen to
-        :param broker_address: ZMQ socket addres for the broker
-        :param logger: Logger instance
-        :param messages: Maximum number of outbound messages. Defaults to infinity.
-        :return:
-        """
-        super(PubConnection, self).__init__(
-            name,
-            listen_to,
-            zmq.PUB,
-            reply=False,
-            broker_address=broker_address,
-            logger=logger,
-            messages=messages
-        )
-
-
-class PubBypassConnection(ComponentBypassOutbound):
+class PushBypassConnection(ComponentBypassOutbound):
     """
     Generic connection that sends a message to a sub service. Good for logs.
     """
@@ -144,12 +93,12 @@ class PubBypassConnection(ComponentBypassOutbound):
         :param logger: Logger instance
         :return:
         """
-        super(PubBypassConnection, self).__init__(name, listen_to, zmq.PUB,
+        super(PushBypassConnection, self).__init__(name, listen_to, zmq.PUSH,
                                                   reply=False, bind=False,
                                                   logger=logger, messages=messages)
 
 
-class SubBypassConnection(ComponentBypassInbound):
+class PullBypassConnection(ComponentBypassInbound):
     """
     Generic connection that opens a Sub socket and bypasses the broker.
     """
@@ -161,6 +110,6 @@ class SubBypassConnection(ComponentBypassInbound):
         :param messages:
         :return:
         """
-        super(SubBypassConnection, self).__init__(name, listen_to, zmq.SUB,
+        super(PullBypassConnection, self).__init__(name, listen_to, zmq.PULL,
                                                   reply=False, bind=False,
                                                   logger=logger, messages=messages)
