26569ef8a56a58957f0b3df39702e9887c662893
Guillem Borrell <guillemborrell@gmail.com>
2016-03-15 10:49:06 +0000

    I have performance counters too.

--- pylm_ng/components/utils.py
+++ pylm_ng/components/utils.py
@@ -3,6 +3,7 @@ from pylm_ng.components.core import zmq_context
 from logging import Handler, Formatter, NOTSET
 import zmq
 import sys
+import time
 
 
 class PushHandler(Handler):
@@ -48,3 +49,52 @@ class LogCollector(object):
         """
         self.nlog += 1
         print('LOG ENTRY #{:<8}:'.format(self.nlog), log_item.decode('utf-8'))
+
+
+class PerformanceCounter(PushBypassConnection):
+    """
+    Class to record and to send performance counters to an external service.
+    """
+    def __init__(self, listen_address):
+        super(PerformanceCounter, self).__init__('perfcounter', listen_address)
+        if sys.version_info[0] < 3:
+            if sys.platform == 'win32':
+                self.timer = time.clock
+            else:
+                self.timer = time.time
+        else:
+            self.timer = time.perf_counter
+
+        self.zero = self.timer()
+
+    def tick(self, label):
+        self.send('{}: {}'.format(label, self.timer()-self.zero).encode('utf-8'))
+
+
+class PerformanceCollector(object):
+    """
+    Endpoint for log collection
+    """
+    def __init__(self, bind_address='inproc://PerformenceCollector'):
+        self.socket = zmq_context.socket(zmq.PULL)
+        self.socket.bind(bind_address)
+        self.bind_address = bind_address
+        self.nlog = 0
+
+    def start(self, messages=sys.maxsize):
+        """
+        Starts the performance counter collector
+        :param messages: Number of messages allowed. Used for debugging.
+        :return:
+        """
+        for i in range(messages):
+            self.emit(self.socket.recv())
+
+    def emit(self, perfcounter):
+        """
+        Emits the performance counter. At this point it is a stupid print
+        :param log_item:
+        :return:
+        """
+        self.nlog += 1
+        print('TICK #{:<8}:'.format(self.nlog), perfcounter.decode('utf-8'))
---
+++ pylm_ng/tests/test_perfcounter.py
@@ -0,0 +1,56 @@
+from pylm_ng.components.core import Broker
+from pylm_ng.components.utils import PerformanceCounter, PerformanceCollector
+from pylm_ng.components.services import RepService
+from pylm_ng.tests.endpoints import ReqConnection, logger
+from threading import Thread
+
+
+def test_perfcounter():
+    collector = PerformanceCollector()
+    perfcounter = PerformanceCounter(listen_address=collector.bind_address)
+
+    perfcounter.tick('Instantiate Broker')
+    broker = Broker(logger=logger, messages=10)
+
+    perfcounter.tick('Instantiate Reply Service')
+    request_reply = RepService('test',
+                               'inproc://repservice',
+                               broker_address=broker.inbound_address,
+                               logger=logger,
+                               messages=10)
+
+    perfcounter.tick('Instantiate Req connection')
+    req_connection = ReqConnection(listen_to=request_reply.listen_address,
+
+                                   logger=logger)
+
+    broker.register_inbound('test', log='Service responds!')
+
+    perfcounter.tick('Launched all threads')
+    t1 = Thread(target=collector.start, args=(7,))
+    t1.start()
+
+    t2 = Thread(target=broker.start)
+    t2.start()
+
+    t3 = Thread(target=request_reply.start)
+    t3.start()
+
+    t4 = Thread(target=req_connection.start)
+    t4.start()
+    perfcounter.tick('Finished launching threads')
+
+    for t in [t2, t3, t4]:
+        t.join()
+
+    perfcounter.tick('Joining threads')
+
+    req_connection.socket.close()
+    broker.inbound.close()
+    broker.outbound.close()
+    perfcounter.tick('Cleaning up')
+
+    t1.join()
+
+if __name__ == '__main__':
+    test_perfcounter()
