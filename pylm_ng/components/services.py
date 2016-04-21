319ed809173edc705cb808fa1c841d3d3b8e4904
Guillem Borrell <guillemborrell@gmail.com>
2016-04-21 10:11:57 +0000

    Added a PushService. Maybe it just works. I don't know.

--- pylm_ng/components/services.py
+++ pylm_ng/components/services.py
@@ -1,6 +1,6 @@
 # The difference between connections and services is that connections
 # connect, while services bind.
-from pylm_ng.components.core import ComponentInbound, zmq_context
+from pylm_ng.components.core import ComponentInbound, ComponentOutbound, zmq_context
 from pylm_ng.components.messages_pb2 import BrokerMessage
 import zmq
 import sys
@@ -53,6 +53,34 @@ class PullService(ComponentInbound):
             name,
             listen_address=listen_address,
             socket_type=zmq.PULL,
+            reply=False,
+            broker_address=broker_address,
+            bind=True,
+            palm=palm,
+            logger=logger,
+            messages=messages
+        )
+
+
+class PushService(ComponentOutbound):
+    """
+    PullService binds to a socket waits for messages from a push-pull queue.
+    """
+    def __init__(self, name, listen_address, broker_address="inproc://broker", palm=False,
+                 logger=None, messages=sys.maxsize):
+        """
+        :param name: Name of the service
+        :param listen_address: ZMQ socket address to bind to
+        :param broker_address: ZMQ socket address of the broker
+        :param logger: Logger instance
+        :param palm: True if service gets PALM messages. False if they are binary
+        :param messages: Maximum number of messages. Defaults to infinity.
+        :return:
+        """
+        super(PushService, self).__init__(
+            name,
+            listen_address=listen_address,
+            socket_type=zmq.PUSH,
             reply=False,
             broker_address=broker_address,
             bind=True,
