61115e3438c6e945ec801bdcd9956b6bd318003f
Guillem Borrell <guillemborrell@gmail.com>
2016-04-25 09:19:24 +0000

    The parallel standalone server works as expected. Yipikayey m*****f******

--- tests/test_standalone/manual_test_client_prototype.py
+++ tests/test_standalone/manual_test_client_prototype.py
@@ -4,6 +4,13 @@ from threading import Thread
 from pylm_ng.standalone import Master, EndPoint, Worker, ParallelClient
 
 
+class NewWorker(Worker):
+    @staticmethod
+    def echo(message):
+        print('Echoing message---')
+        return message
+
+
 def test_standalone_parallel_client():
     this_log_address = "inproc://log6"
     this_perf_address = "inproc://perf6"
@@ -17,18 +24,18 @@ def test_standalone_parallel_client():
                     endpoint.log_address, endpoint.perf_address,
                     endpoint.ping_address, palm=True)
 
-    worker1 = Worker('worker1',
-                     master.worker_push_address,
-                     master.worker_pull_address,
-                     this_log_address,
-                     this_perf_address,
-                     this_ping_address)
-    worker2 = Worker('worker2',
-                     master.worker_push_address,
-                     master.worker_pull_address,
-                     this_log_address,
-                     this_perf_address,
-                     this_ping_address)
+    worker1 = NewWorker('worker1',
+                        master.worker_push_address,
+                        master.worker_pull_address,
+                        this_log_address,
+                        this_perf_address,
+                        this_ping_address)
+    worker2 = NewWorker('worker2',
+                        master.worker_push_address,
+                        master.worker_pull_address,
+                        this_log_address,
+                        this_perf_address,
+                        this_ping_address)
 
     client = ParallelClient(master.push_address,
                             master.pull_address,
@@ -45,11 +52,11 @@ def test_standalone_parallel_client():
         t.start()
 
     def message_generator():
-        for i in range(10):
-            yield str(i).encode('utf-8')
+        for j in range(10):
+            yield str(j).encode('utf-8')
 
     print('******* Launch client')
-    for i, m in enumerate(client.job('somefunction', message_generator(), 10)):
+    for i, m in enumerate(client.job('echo', message_generator(), 10)):
         print('************ Got something back', m, i)
 
     for t in threads:
