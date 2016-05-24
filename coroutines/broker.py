9c7dbfe022fc222bdc60f17d79ed19d4335b6618
Guillem Borrell <guillemborrell@gmail.com>
2016-05-24 13:24:20 +0000

    Components are not instantiated by the broker.

--- coroutines/broker.py
+++ coroutines/broker.py
@@ -5,16 +5,25 @@ import sys
 
 
 class Broker(object):
+    """
+    Pylm broker that does not use the ZeroMQ polling mechanism, but
+    generators and coroutines to wire the connectors.
+    """
+    # Asyncio event loop.
     event_loop = asyncio.get_event_loop()
+    # Mapping of outbound connections as generators
     outbound = {}
-    connections = {}
+    # Mapping of components (component, outbound)
+    components = {}
+    # List of futures for the connections
     futures = []
+    # Message counter. It has to be a class attribute.
     counter = 0
 
     def __init__(self, messages=sys.maxsize):
         self.messages = messages
 
-    def looping(self):
+    def loop(self):
         while self.counter < self.messages:
             recv = (yield)
             print(recv, self.counter)
@@ -26,18 +35,19 @@ class Broker(object):
         self.event_loop.close()
 
     @property
-    def gen(self):
-        generator = self.looping()
+    def handler(self):
+        generator = self.loop()
         generator.send(None)
         return generator
 
-    def register_inbound(self, connector, name, outbound=None, messages=sys.maxsize):
-        connector_instance = connector(name, self.gen)
-        self.futures.append(asyncio.ensure_future(connector_instance.start(messages)))
+    def register_inbound(self, component, outbound=None, messages=sys.maxsize):
+        self.components[component.name] = (component, outbound)
+        self.futures.append(asyncio.ensure_future(component.start(messages)))
 
-    def register_outbound(self, connector, name, messages=sys.maxsize):
-        connector_instance = connector(name, self.gen)
-        self.futures.append(asyncio.ensure_future(connector_instance.start(messages)))
+    def register_outbound(self, component, messages=sys.maxsize):
+        self.outbound[component.name] = component
+        self.components[component.name] = (component, None)
+        self.futures.append(asyncio.ensure_future(component.start(messages)))
 
     def start(self):
         print("Starting event loop.")
@@ -62,7 +72,6 @@ class ConnectorInbound(object):
             self.events.send(self.name)
 
         print('done {}'.format(self.name))
-        return b'0'
 
 
 class ConnectorOutbound(object):
@@ -71,11 +80,11 @@ class ConnectorOutbound(object):
         self.events = events
 
     async def start(self, messages=sys.maxsize):
-        for i in range(20):
-            pass
+        for i in range(messages):
+            print(self.name)
 
 
-broker = Broker(messages=20)
-broker.register_inbound(ConnectorInbound, 'connector1', messages=10)
-broker.register_inbound(ConnectorInbound, 'connector2', messages=10)
+broker = Broker(messages=10)
+broker.register_inbound(ConnectorInbound('connector1', broker.handler), messages=5)
+broker.register_inbound(ConnectorInbound('connector2', broker.handler), messages=5)
 broker.start()
