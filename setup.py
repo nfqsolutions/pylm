13e1084c376a7821860bb029ec930a4112270091
Guillem Borrell <guillemborrell@gmail.com>
2016-06-09 13:27:22 +0000

    Version bump. Pylm is now much more stable.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pylm',
-      version='0.4',
+      version='0.5',
       description='New Python prototype of PALM pipelines',
       author='Guillem Borrell',
       author_email='guillemborrell@gmail.com',
