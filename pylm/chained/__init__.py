34e12c9a867e70eb1aa4de5df3629e98a8a8853e
Guillem Borrell <guillemborrell@gmail.com>
2016-06-14 10:37:13 +0000

    Now it should be able to chain the valuation, since there is a chained ValuationMaster. It has not been tested yet. palm_standalone has been refactored to standalone.

--- pylm/chained/__init__.py
+++ pylm/chained/__init__.py
@@ -1,3 +1,6 @@
 """
 Series of servers and clients that can be chanied manually.
 """
+from pylm.chained.client import Client, ParallelClient
+from pylm.chained.servers import Master, Worker
+from pylm.chained.endpoints import EndPoint
