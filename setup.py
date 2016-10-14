ff3f8f5f7447be8ab86c742b14bf10fcb213d666
Guillem Borrell <guillemborrell@gmail.com>
2016-10-14 15:00:28 +0200

    Updated long description to avoid adding a missing image.

--- setup.py
+++ setup.py
@@ -5,8 +5,44 @@ from setuptools import setup
 with open('pylm/version.py') as f:
     exec(f.read())
 
-with open('README.rst') as g:
-    long_description = g.read()
+
+long_description = """
+Pylm
+====
+
+Pylm is the Python implementation of PALM, a framework to build
+clusters of high performance components. It is presented in two
+different levels of abstraction. In the high level API you will find
+servers and clients that are functional *out of the box*. Use the high
+level API if you are interested in simple communication patterns like
+client-server, master-slave or a streaming pipeline. In the low level
+API there are a variety of small components that, once combined,
+they can be used to implement almost any kind of
+component. It's what the high level API uses under the hood. Choose
+the low level API if you are interested in creating your custom
+component and your custom communication pattern.
+
+**Pylm requires a version of Python equal or higher than 3.4, and it is
+more thoroughly tested with Python 3.5.**
+
+Installing **pylm** is as easy as:
+
+.. code-block:: bash
+
+   $> pip install pylm
+
+* `PYPI package page <https://pypi.python.org/pypi/pylm/>`_
+
+* `Documentation <http://pylm.readthedocs.io/en/latest/>`_
+
+* `Source code <https://github.com/nfqsolutions/pylm>`_
+
+Pylm is released under a dual licensing scheme. The source is released
+as-is under the the AGPL version 3 license, a copy of the license is
+included with the source. If this license does not suit you,
+you can purchase a commercial license from `NFQ Solutions
+<http://nfqsolutions.com>`_
+"""
     
 setup(name='pylm',
       version=__version__,
