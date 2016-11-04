699dbbf0727d8cc18110b7c239dc649f418dd2c3
Guillem Borrell <guillemborrell@gmail.com>
2016-11-04 11:45:46 +0100

    Changed name client to clients for symmetry

--- pylm/chained/__init__.py
+++ pylm/chained/__init__.py
@@ -17,7 +17,7 @@
 """
 Series of servers and clients that can be chanied manually.
 """
-from pylm.chained.client import Client, LoopClient
+from pylm.chained.clients import Client, LoopClient
 from pylm.chained.servers import Master, Server, LastServer
 from pylm.standalone.servers import Worker
 from pylm.chained.endpoints import EndPoint
