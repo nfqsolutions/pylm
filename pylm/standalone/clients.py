64abeb02fb29669ba353ae9e56eb8d7d6755ebbe
Guillem Borrell <guillemborrell@gmail.com>
2016-11-24 18:12:20 +0100

    Added the SubscribedClient with tests.
    This client will be the base of the definitive parallel client.
    
    Test does not work as expected. Staging.
    
    Wow. The dump example does not work.
    
    Now at least the fake client works.
    
    Subscribed client works rudimentarily
    
    Subscribed client works with a non trivial message
    
    Cleaned up the test.

--- pylm/standalone/clients.py
+++ pylm/standalone/clients.py
@@ -19,9 +19,9 @@ from pylm.parts.messages_pb2 import PalmMessage
 from threading import Thread
 from uuid import uuid4
 from itertools import repeat
+import concurrent.futures
 import zmq
 import sys
-import time
 
 
 class Client(object):
@@ -119,6 +119,126 @@ class Client(object):
         self.req.send(message.SerializeToString())
         message.ParseFromString(self.req.recv())
         return message.payload
+
+
+class SubscribedClient(object):
+    """
+    Client to connect to parallel servers
+
+    :param sub_address: Address of the pub service of the server to subscribe to
+    :param pull_address: Address of the push service of the server to pull from
+    :param db_address: Address for the cache service
+    :param server_name: Name of the server to be connected to
+    :param pipeline: Name of the pipeline if the session has to be reused
+    """
+    def __init__(self, sub_address: str, pull_address: str,
+                 db_address: str, server_name: str,
+                 pipeline: str = None):
+        self.sub_address = sub_address
+        self.pull_address = pull_address
+        self.db_address = db_address
+        self.server_name = server_name
+        if pipeline:
+            self.pipeline = pipeline
+        else:
+            self.pipeline = str(uuid4())
+        self.uuid = str(uuid4())
+
+    def clean(self):
+        self.push.close()
+        self.sub.close()
+        self.db.close()
+
+    def _sender(self, socket, function, generator):
+        for payload in generator:
+            message = PalmMessage()
+            message.function = function
+            message.stage = 1
+            message.pipeline = self.pipeline
+            message.client = self.uuid
+            message.payload = payload
+
+            socket.send(message.SerializeToString())
+        
+    def job(self, function, generator, cache=None, messages=sys.maxsize):
+        push_socket = zmq_context.socket(zmq.PUSH)
+        push_socket.connect(self.pull_address)
+        sender_thread = Thread(target=self._sender,
+                               args=(push_socket, function, generator))
+
+        # Sender runs in background.
+        sender_thread.start()
+        
+        sub_socket = zmq_context.socket(zmq.SUB)
+        sub_socket.setsockopt_string(zmq.SUBSCRIBE, self.uuid)
+        sub_socket.connect(self.sub_address)
+
+        for i in range(messages):
+            [client, message] = sub_socket.recv_multipart()
+            if not client.decode('utf-8') == self.uuid:
+                raise ValueError('The client got a message that does not belong')
+
+            yield message 
+
+
+    def set(self, value: bytes, key=None):
+        """
+        Sets a key value pare in the remote database. If the key is not set,
+        the function returns a new key. Note that the order of the arguments
+        is reversed from the usual
+
+        :param value: Value to be stored
+        :param key: Key for the k-v storage
+        :return: New key or the same key
+        """
+        if not type(value) == bytes:
+            raise TypeError('First argument {} must be of type <bytes>'.format(value))
+
+        message = PalmMessage()
+        message.pipeline = str(uuid4())  # For a set job, the pipeline is not important
+        message.client = self.uuid
+        message.stage = 0
+        message.function = '.'.join([self.server_name, 'set'])
+        message.payload = value
+        if key and self.session_set:
+            message.cache = ''.join([self.pipeline, key])
+        elif key:
+            message.cache = key
+
+        self.db.send(message.SerializeToString())
+        return self.db.recv().decode('utf-8')
+
+    def get(self, key):
+        """
+        Gets a value from server's internal cache
+
+        :param key: Key for the data to be selected.
+        :return: Value
+        """
+        message = PalmMessage()
+        message.pipeline = str(uuid4())
+        message.client = self.uuid
+        message.stage = 0
+        message.function = '.'.join([self.server_name, 'get'])
+        message.payload = key.encode('utf-8')
+        self.db.send(message.SerializeToString())
+        return self.db.recv()
+
+    def delete(self, key):
+        """
+        Deletes data in the server's internal cache.
+
+        :param key: Key of the data to be deleted
+        :return:
+        """
+        message = PalmMessage()
+        message.pipeline = str(uuid4())
+        message.client = self.uuid
+        message.stage = 0
+        message.function = '.'.join([self.server_name, 'delete'])
+        message.payload = key.encode('utf-8')
+        self.db.send(message.SerializeToString())
+        return self.db.recv().decode('utf-8')
 
 
 class ParallelClient(object):
---
+++ tests/test_standalone/test_subscribed_client.py
@@ -0,0 +1,55 @@
+from pylm.parts.core import zmq_context
+from pylm.parts.messages_pb2 import PalmMessage
+from pylm.standalone.clients import SubscribedClient
+from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
+import zmq
+
+
+def fake_server(messages=1):
+    db_socket = zmq_context.socket(zmq.REP)
+    db_socket.bind('inproc://db')
+
+    pull_socket = zmq_context.socket(zmq.PULL)
+    pull_socket.bind('inproc://pull')
+
+    pub_socket = zmq_context.socket(zmq.PUB)
+    pub_socket.bind('inproc://pub')
+
+    for i in range(messages):
+        message_data = pull_socket.recv()
+        message = PalmMessage()
+        message.ParseFromString(message_data)
+
+        topic = message.client
+        pub_socket.send_multipart([topic.encode('utf-8'), message_data])
+        print('Server sent message')
+
+
+client = SubscribedClient(
+    sub_address='inproc://pub',
+    pull_address='inproc://pull',
+    db_address='inproc://db',
+    server_name='someserver',
+    pipeline=None)
+
+
+def test_subscribed_client_single():
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        results = [
+            executor.submit(fake_server, messages=2),
+            executor.submit(client.job, 'f', [b'1', b'2'], messages=2)
+        ]
+        for future in concurrent.futures.as_completed(results):
+            try:
+                result = future.result()
+                if result:
+                    for r in result:
+                        print(r)
+
+            except Exception as exc:
+                print(exc)
+
+
+if __name__ == '__main__':
+    test_subscribed_client_single()
