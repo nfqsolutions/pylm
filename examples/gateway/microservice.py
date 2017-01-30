a709d131b947b1a4f9cb76ccadcbbbb4fa7e6882
Guillem Borrell <guillemborrell@gmail.com>
2017-01-30 11:18:04 +0100

    Updated the gateway with the new internals.

--- examples/gateway/microservice.py
+++ examples/gateway/microservice.py
@@ -10,39 +10,26 @@ worker_pull_address = 'tcp://127.0.0.1:5557'
 worker_push_address = 'tcp://127.0.0.1:5558'
 db_address = 'tcp://127.0.0.1:5559'
 
-cache = DictDB()
-cache.set('name', b'server')
-cache.set('worker_pull_address', 'tcp://127.0.0.1:5557'.encode('utf-8'))
-cache.set('worker_push_address', 'tcp://127.0.0.1:5558'.encode('utf-8'))
-
 server.register_inbound(GatewayRouter,
                         'gateway_router',
                         'inproc://gateway_router',
-                        route='WorkerPush',
-                        cache=cache)
+                        route='WorkerPush')
 server.register_outbound(GatewayDealer,
                          'gateway_dealer',
-                         listen_address='inproc://gateway_router',
-                         cache=cache)
+                         listen_address='inproc://gateway_router')
 server.register_bypass(HttpGateway,
                        name='HttpGateway',
                        listen_address='inproc://gateway_router',
                        hostname='localhost',
-                       port=8888,
-                       cache=cache)
-server.register_inbound(WorkerPullService,
-                        'WorkerPull',
-                        worker_pull_address,
-                        route='gateway_dealer',
-                        cache=cache)
-server.register_outbound(WorkerPushService,
-                         'WorkerPush',
-                         worker_push_address,
-                         cache=cache)
-server.register_bypass(CacheService,
-                       'Cache',
-                       db_address,
-                       cache=cache)
+                       port=8888)
+server.register_inbound(WorkerPullService, 'WorkerPull', worker_pull_address, route='gateway_dealer')
+server.register_outbound(WorkerPushService, 'WorkerPush', worker_push_address)
+server.register_bypass(CacheService, 'Cache', db_address)
+
+server.preset_cache(name='server',
+                    db_address=db_address,
+                    worker_pull_address=worker_pull_address,
+                    worker_push_address=worker_push_address)
 
 if __name__ == '__main__':
     server.start()
