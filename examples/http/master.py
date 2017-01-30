73ba69695de4b63e011a99b6ec3cd726f88fedf9
Guillem Borrell <guillemborrell@gmail.com>
2017-01-30 11:13:31 +0100

    Updated the HTTP server with the new internals.

--- examples/http/master.py
+++ examples/http/master.py
@@ -1,4 +1,3 @@
-from pylm.persistence.kv import DictDB
 from pylm.parts.servers import ServerTemplate
 from pylm.parts.services import PullService, PubService
 from pylm.parts.connections import HttpConnection
@@ -8,19 +7,16 @@ server = ServerTemplate()
 
 db_address = 'tcp://127.0.0.1:5559'
 pull_address = 'tcp://127.0.0.1:5555'
-push_address = 'tcp://127.0.0.1:5556'
+pub_address = 'tcp://127.0.0.1:5556'
 
-cache = DictDB()
-cache.set('name', b'server')
-cache.set('db_address', b'tcp://127.0.0.1:5559')
-cache.set('pull_address', b'tcp://127.0.0.1:5555')
-cache.set('pub_address', b'tcp://127.0.0.1:5556')
-
-
-server.register_inbound(PullService, 'Pull', pull_address, route='HttpConnection', cache=cache)
-server.register_outbound(HttpConnection, 'HttpConnection', 'http://localhost:8888', route='Pub', cache=cache)
-server.register_outbound(PubService, 'Pub', push_address, cache=cache)
-server.register_bypass(CacheService, 'Cache', db_address, cache=cache)
+server.register_inbound(PullService, 'Pull', pull_address, route='HttpConnection')
+server.register_outbound(HttpConnection, 'HttpConnection', 'http://localhost:8888', route='Pub')
+server.register_outbound(PubService, 'Pub', pub_address)
+server.register_bypass(CacheService, 'Cache', db_address)
+server.preset_cache(name='server',
+                    db_address=db_address,
+                    pull_address=pull_address,
+                    pub_address=pub_address)
 
 if __name__ == '__main__':
     server.start()
